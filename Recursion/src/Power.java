dc3ea1d6e328f20d00380f97faac48ef46db7270
HugoSanche <hbmclaren@gmail.com>
2024-05-22 15:49:35 -0600

    add exercise Power

--- Recursion/src/Power.java
+++ Recursion/src/Power.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Power {
+public class Power {
+    int mul=1;
+    public static void main(String[] args) {
+        int base=5;
+        int exponent=3;
+
+      Power power=new Power();
+        int res=power.calculatePower(base,exponent);
+        System.out.println(res);
+    }
+    public  int calculatePower(int base, int exponent){
+        if (exponent<1){
+            return -1;
+        }
+        if(exponent==0){
+            return 1;
+        }
+     //base*calculatePower(base,exponent-1);
+        return base*calculatePower(base,exponent-1);
+    }
 }
