bf500211151d205a9002de7bb1fb66a260dfee15
HugoSanche <hbmclaren@gmail.com>
2024-05-22 15:49:57 -0600

    add exercise Sum of digitis

--- Recursion/src/SumOfDigits.java
+++ Recursion/src/SumOfDigits.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SumOfDigits {
+public class SumOfDigits {
+    int sum;
+    int mod;
+    public static void main(String[] args) {
+        SumOfDigits sum=new SumOfDigits();
+        int resultado=sum.sumOfDigits(436);
+        System.out.println(resultado);
+    }
+    public int sumOfDigits(int n){
+        if (n==0){
+            return 0;
+        }
+        //mod=n%10;
+      //  sum=sum+mod+sumOfDigits(n/10);
+        return n%10+sumOfDigits(n/10);
+    }
 }
